0150bcaeee8f5a52d2f6df5c62450f8dd70e9fe0
unknown <roman.kravchenko@kravchenko-rn.metinvest.ua>
2015-11-18 11:44:09 +0200

    Добавлена дата/время сообщения

--- Message.java
+++ Message.java
@@ -1,10 +1,12 @@
 package lecture12.mySocketChat;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Message implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    private Date dateTime;
     private String text;
     private String from;
 
